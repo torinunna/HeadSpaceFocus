d81396fe216124426a1e02d3c607ba007e06a1c3
torinunna <torinunna9588@gmail.com>
2023-05-12 15:37:50 +0900

    create FocusVC

---
+++ HeadSpaceFocus/FocusViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FocusViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by YUJIN KWON on 2023/05/12.
+//
+
+import UIKit
+
+class FocusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
