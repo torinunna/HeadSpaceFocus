6983ae7735cba197a62eb425eabb44ec7467f7a2
torinunna <torinunna9588@gmail.com>
2023-05-23 22:59:13 +0900

    create QuickFocus.storyboard and QuickFocusListVC

---
+++ HeadSpaceFocus/QuickFocusListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuickFocusListViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by YUJIN KWON on 2023/05/23.
+//
+
+import UIKit
+
+class QuickFocusListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+}
