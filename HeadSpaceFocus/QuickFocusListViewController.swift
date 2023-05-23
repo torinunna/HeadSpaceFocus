4e96edd4f17e953247af94150425851f9a6cb09e
torinunna <torinunna9588@gmail.com>
2023-05-23 23:11:33 +0900

    add collectionaView without collectionViewCell to QuickFocusDetailVC

--- HeadSpaceFocus/QuickFocusListViewController.swift
+++ HeadSpaceFocus/QuickFocusListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class QuickFocusListViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
